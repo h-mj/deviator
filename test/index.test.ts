845175d320cf8ef53257801baa4de12644a04c80
Henri-Martin Jaakson <h.m.jaakson@gmail.com>
2019-08-22 13:30:17 +0300

    test: add string deviator tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,23 @@
-import { returnHello } from "../src";
+import { deviate } from "../src";
 
-it('returns "Hello"', () => {
-  expect(returnHello()).toBe("Hello");
+it("trims and converts a string to a float", () => {
+  const deviation = deviate<string>()
+    .trim()
+    .toNumber();
+
+  const values = {
+    "      69.1        ": 69.1,
+    "         11": 11.0,
+    "    a": false,
+    "400     ": 400,
+    " .1a  ": false
+  };
+
+  for (const entry of Object.entries(values)) {
+    const result = deviation(entry[0]);
+    const expectedError = entry[1] === false;
+
+    expect(result.kind).toBe(expectedError ? "Err" : "Next");
+    expect(result.value).toBe(expectedError ? "not_a_number" : entry[1]);
+  }
 });
