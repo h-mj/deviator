26be2d1b1f63c7e6525d688c6e0a683d070f2af7
Henri-Martin Jaakson <h.m.jaakson@gmail.com>
2019-10-26 00:04:00 +0300

    feat: object shaping deviation

--- src/deviator/ObjectDeviator.ts
+++ src/deviator/ObjectDeviator.ts
@@ -1,7 +1,57 @@
+import { Deviation, Failure, Success } from "../Deviation";
+import { err, ok } from "../result";
 import { BaseDeviator } from "./BaseDeviator";
+
+/**
+ * Type which is used to transform an object of type `O` into some other object.
+ */
+// prettier-ignore
+type Schema<O extends object, S extends Schema<O, S>> = {
+  [P in keyof S]: S[P] extends Deviation<P extends keyof O ? O[P] : unknown, unknown, unknown>
+    ? S[P]
+    : never;
+};
+
+/**
+ * Successful shaping result value type.
+ */
+type ShapeOk<S extends Schema<object, object>> = { [P in keyof S]: Success<S> };
+
+/**
+ * Unsuccessful shaping result value type.
+ */
+type ShapeErr<S extends Schema<object, object>> = {
+  [P in keyof S]?: Failure<S>;
+};
 
 /**
  * Deviation builder class which intermediate value extends `object`.
  */
 // prettier-ignore
-export class ObjectDeviator<I, O extends object, E> extends BaseDeviator<I, O, E> {}
+export class ObjectDeviator<I, O extends object, E> extends BaseDeviator<I, O, E> {
+  /**
+   * Transforms some of the properties of current object value using deviations
+   * defined in specified `schema`.
+   */
+  public shape<S extends Schema<O, S>>(schema: S) {
+    return this.then(input => {
+      const value: ShapeOk<S> = {} as ShapeOk<S>;
+      const errors: ShapeErr<S> = {};
+
+      for (const property in schema) {
+        // @ts-ignore
+        const result = schema[property](input[property]);
+
+        if (result.ok) {
+          // @ts-ignore
+          value[property] = result.value;
+        } else {
+          // @ts-ignore
+          errors[property] = result.value;
+        }
+      }
+
+      return Object.keys(errors).length === 0 ? ok(value) : err(errors);
+    });
+  }
+}
