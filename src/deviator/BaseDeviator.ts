18a4671d4962959a467af0e5729c73b66c01ae5b
Henri-Martin Jaakson <h.m.jaakson@gmail.com>
2019-10-26 14:33:12 +0300

    refactor: simplify or deviation

--- src/deviator/BaseDeviator.ts
+++ src/deviator/BaseDeviator.ts
@@ -1,6 +1,6 @@
-import { Deviation } from "../Deviation";
-import { ok } from "../result";
-import { deviator, Deviator } from "./Deviator";
+import { Deviation, Success } from "../Deviation";
+import { err, ok } from "../result";
+import { deviator } from "./Deviator";
 
 // eslint-disable-next-line
 export interface BaseDeviator<I, O, N, E> extends Deviation<I, O, N, E> {}
@@ -8,37 +8,26 @@ export interface BaseDeviator<I, O, N, E> extends Deviation<I, O, N, E> {}
 /**
  * Base deviation builder type.
  */
-// prettier-ignore
 export class BaseDeviator<I, O, N, E> {
   /**
-   * Deviates current intermediate value first by `first` deviation and if that
-   * fails with subsequent deviations and returns the successful result with
-   * value of first successful deviation value. If all specified deviations
-   * fail, result of the `first` deviation is returned.
+   * If any specified deviations deviate successfully current intermediate
+   * value, returns successful result with returned result value. If all
+   * specified deviations fail, error result with `"or"` value is returned.
    */
-  public or<O2 = never, N2 = never, E2 = never, Os = never, Ns = never, Es = never>(
-    first: Deviation<O, O2, N2, E2>,
-    ...deviations: Deviation<O, Os, Ns, Es>[]
-  ): Deviator<I, O2 | N2 | Os | Ns, N, E | E2> {
-    const choice: Deviation<O, O2 | N2 | Os | Ns, never, E2> = input => {
-      const initial = first(input);
-
-      if (initial.ok) {
-        return ok(initial.value);
-      }
-
+  public or<Ds extends Deviation<unknown, unknown, unknown, unknown>[]>(
+    ...deviations: Ds
+  ) {
+    return this.then(input => {
       for (const deviation of deviations) {
         const result = deviation(input);
 
         if (result.ok) {
-          return ok(result.value);
+          return ok(result.value as Success<Ds[number]>);
         }
       }
 
-      return initial;
-    };
-
-    return this.then(choice);
+      return err("or" as const);
+    });
   }
 
   /**
