eebcd36ec3c3d5aa6af0035a0fc26f9c9ac7890c
Henri-Martin Jaakson <h.m.jaakson@gmail.com>
2019-10-26 14:18:33 +0300

    feat: base.or deviation, make array.each require only one argument

--- src/deviator/ArrayDeviator.ts
+++ src/deviator/ArrayDeviator.ts
@@ -8,38 +8,26 @@ import { BaseDeviator } from "./BaseDeviator";
 // prettier-ignore
 export class ArrayDeviator<I, O extends unknown[], N, E> extends BaseDeviator<I, O, N, E> {
   /**
-   * Deviates each value in the array first by `first` deviation and if that
-   * fails with subsequent deviations and replaces said value with the value of
-   * first successful deviation result. If all specified deviations fail, result
-   * value of the first deviation is assigned to the same index as the deviated
-   * value.
+   * Deviates each value in the array first by specified deviation and either
+   * returns same sized array where elements at given index have the value of
+   * the successful deviation result, or sparse array where values at given
+   * index are the unsuccessful result values.
    */
-  public each<O2 = never, N2 = never, E2 = never, Os = never, Ns = never, Es = never>(
-    first: Deviation<O[number], O2, N2, E2>,
-    ...deviations: Deviation<O[number], Os, Ns, Es>[]
+  public each<O2 = never, N2 = never, E2 = never>(
+    deviation: Deviation<O[number], O2, N2, E2>
   ) {
     return this.then(input => {
-      const array: Array<O2 | Os | N2 | Ns> = new Array(input.length);
-      const errors: Array<E2 | Es | undefined> = new Array(input.length);
+      const array: Array<O2 | N2> = new Array(input.length);
+      const errors: Array<E2 | undefined> = new Array(input.length);
 
-      outer: for (let i = 0; i < input.length; ++i) {
-        const firstResult = first(input[i]);
+      for (let i = 0; i < input.length; ++i) {
+        const result = deviation(input[i]);
 
-        if (firstResult.ok) {
-          array[i] = firstResult.value;
-          continue;
+        if (result.ok) {
+          array[i] = result.value;
+        } else {
+          errors[i] = result.value;
         }
-
-        for (const deviation of deviations) {
-          const result = deviation(input[i]);
-
-          if (result.ok) {
-            array[i] = result.value;
-            continue outer;
-          }
-        }
-
-        errors[i] = firstResult.value;
       }
 
       return errors.find(error => error !== undefined) === undefined
--- src/deviator/BaseDeviator.ts
+++ src/deviator/BaseDeviator.ts
@@ -1,6 +1,6 @@
 import { Deviation } from "../Deviation";
 import { ok } from "../result";
-import { deviator } from "./Deviator";
+import { deviator, Deviator } from "./Deviator";
 
 // eslint-disable-next-line
 export interface BaseDeviator<I, O, N, E> extends Deviation<I, O, N, E> {}
@@ -8,7 +8,39 @@ export interface BaseDeviator<I, O, N, E> extends Deviation<I, O, N, E> {}
 /**
  * Base deviation builder type.
  */
+// prettier-ignore
 export class BaseDeviator<I, O, N, E> {
+  /**
+   * Deviates current intermediate value first by `first` deviation and if that
+   * fails with subsequent deviations and returns the successful result with
+   * value of first successful deviation value. If all specified deviations
+   * fail, result of the `first` deviation is returned.
+   */
+  public or<O2 = never, N2 = never, E2 = never, Os = never, Ns = never, Es = never>(
+    first: Deviation<O, O2, N2, E2>,
+    ...deviations: Deviation<O, Os, Ns, Es>[]
+  ): Deviator<I, O2 | N2 | Os | Ns, N, E | E2> {
+    const choice: Deviation<O, O2 | N2 | Os | Ns, never, E2> = input => {
+      const initial = first(input);
+
+      if (initial.ok) {
+        return ok(initial.value);
+      }
+
+      for (const deviation of deviations) {
+        const result = deviation(input);
+
+        if (result.ok) {
+          return ok(result.value);
+        }
+      }
+
+      return initial;
+    };
+
+    return this.then(choice);
+  }
+
   /**
    * Sets current intermediate value to specified `value`.
    */
